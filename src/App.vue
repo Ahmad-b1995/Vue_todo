5015c9ed2a93d99d9257e685dc582643c04c1f83
Ahmad <ahmad.b1995@gmail.com>
2021-04-16 17:47:31 +0430

    app structure is set

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <input class="input" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  date(){
+    return{
+      list:[]
+    }
+  },
+  methods:{
+    add_to_list:{
+
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+.input{
+  margin: 50px auto;
+display: block;
+}
+</style>
