1a664436972c75821e5b1e9d6bed2c6d3a1f618a
Ahmad <ahmad.b1995@gmail.com>
2021-04-16 18:27:31 +0430

    almost done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <input class="input" type="text">
+    <input class="input" ref="input" @change="add_to_list" type="text" />
+    <ul class="list">
+      <template v-for="(item, index) in list">
+        <input
+          type="checkbox"
+          id="item"
+          :name="item"
+          :value="item"
+          :key="index"
+        />
+        <label :class="strike_through" for="vehicle1" :key="`label${index}`">
+          {{ item }}</label
+        >
+        
+        <br :key="`br${index}`" />
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  date(){
-    return{
-      list:[]
-    }
+  data() {
+    return {
+      list: [],
+    };
   },
-  methods:{
-    add_to_list:{
-
-    }
-  }
+  methods: {
+    add_to_list: function (event) {
+      this.list.push(event.target.value);
+      this.$refs.input.value = "";
+    },
+  },
 };
 </script>
 
@@ -26,8 +43,18 @@ export default {
   text-align: center;
   margin-top: 60px;
 }
-.input{
+.input {
   margin: 50px auto;
-display: block;
+  display: block;
+}
+
+.list {
+  width: 150px;
+  margin: 20px auto;
+  text-align: left;
+}
+
+.strike_through {
+  text-decoration: line-through;
 }
 </style>
