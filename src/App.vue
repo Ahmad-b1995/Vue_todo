ee0b728496f5db7d45897954eb47b95e0cacd57f
Ahmad <ahmad.b1995@gmail.com>
2021-05-23 18:40:00 +0430

    added checked or unckecked to the array

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,20 @@
       <template v-for="(item, index) in list">
         <input
           type="checkbox"
-          id="item"
+          :id="index"
           :name="item"
-          :value="item"
           :key="index"
+          :checked="item.done"
         />
-        <label :class="strike_through" for="vehicle1" :key="`label${index}`">
-          {{ item }}</label
+        <label
+          :id="index"
+          :class="strike_through"
+          for="vehicle1"
+          :key="`label${index}`"
         >
-        
+          {{ item.title }}</label
+        >
+
         <br :key="`br${index}`" />
       </template>
     </ul>
@@ -31,7 +36,7 @@ export default {
   },
   methods: {
     add_to_list: function (event) {
-      this.list.push(event.target.value);
+      this.list.unshift({ 'title': event.target.value, 'done': false });
       this.$refs.input.value = "";
     },
   },
